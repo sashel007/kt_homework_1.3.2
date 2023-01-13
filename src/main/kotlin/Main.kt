841b62af92483baf73a220a77f4974b2d3e94068
Alexander Sidorkin <sashel007@yandex.ru>
2023-01-13 18:51:49 +0300

    itinial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,61 @@
+fun main() {
+
+    discount(1,"Mastercard", 40)
+    discount(1,"Maestro", 23333)
+    discount(1,"Visa", 414)
+//    discount(2,"Мир", 1233312)
+//    discount(3,"VK Pay", 403)
+//    discount(1,"Maestro", 23933)
+
+}
+
+const val MAX_DAY_LIMIT = 150_000
+const val MAX_MONTH_LIMIT = 600_000
+
+// массив из 30 дней, куда вносится сумма перевода за определенные дни
+var month = Array(30) { 0 }
+
+fun discount(day: Int, cardType: String, currentSum: Int) {
+
+    //проверим, не превышен ли лимит за месяц
+    if ((currentSum + checkMonthSum()) <= MAX_MONTH_LIMIT) {
+        //если не превышен, то проверим, не превышен ли лимит за указанный день
+        val sumForDay = 0
+        month[day] = sumForDay
+
+        if ((currentSum + sumForDay) <= MAX_DAY_LIMIT) {
+            //вложим сумму перевода в ячейку массива, отвечающую за сумму всех транзакций в течение указанного дня
+            month[day] = day + currentSum
+            //рассчитаем скидку
+            when (cardType) {
+                "Mastercard" -> if (currentSum in 300..75000) "$currentSum" else "Сумма со скидкой" +
+                        "составляет: ${currentSum * 0.94 + 20}"
+                "Maestro" -> if (currentSum in 300..75000) "$currentSum" else "Сумма со скидкой" +
+                        "составляет: ${currentSum * 0.94 + 20}"
+                "Visa" -> if (currentSum >= 35) {
+                                "Сумма со скидкой составляет: ${currentSum * 0.925}"
+                            } else {
+                                currentSum == 0
+                                println("Минимальная сумма должна составлять 35 рублей")
+                            }
+                "Мир" -> if (currentSum >= 35) {
+                    "Сумма со скидкой составляет: ${currentSum * 0.925}"
+                    } else {
+                        currentSum == 0
+                        println("Минимальная сумма должна составлять 35 рублей")
+                    }
+                "VK Pay" -> "$currentSum"
+                else -> "Неверно указана платежная система"
+            }
+        } else {
+            print("Лимит за день превышен")
+        }
+    } else {
+        print("Лимит за месяц превышен превышен")
+    }
+}
+
+// метод, проверяющий какая сумма была уже переведена за месяц
+fun checkMonthSum(): Int {
+    return month.sum()
+}
